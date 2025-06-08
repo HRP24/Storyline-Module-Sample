764bceda324a8372231ab5cc56fbe1397452a478
HRP24 <hemantpawar2688@gmail.com>
2025-06-08 20:27:38 +0530

    Add files via upload

---
+++ story_content/triggers.js
@@ -0,0 +1,119 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "62dFyPoakzs":
+        Script1();
+        break;
+      case "6jJJYvjwMPY":
+        Script2();
+        break;
+      case "5ifBVvFmhdC":
+        Script3();
+        break;
+      case "6YUAJoLPCl1":
+        Script4();
+        break;
+      case "6kUwhM6amp6":
+        Script5();
+        break;
+      case "5vRjo9RUGrl":
+        Script6();
+        break;
+      case "5oQ11gldjYn":
+        Script7();
+        break;
+      case "6RVlWKwXogN":
+        Script8();
+        break;
+      case "61vb5cVAuMl":
+        Script9();
+        break;
+      case "64sdi8jAmyx":
+        Script10();
+        break;
+      case "679oJHtpHDx":
+        Script11();
+        break;
+      case "62UG6qidFDm":
+        Script12();
+        break;
+      case "6Ko2RSIYqPy":
+        Script13();
+        break;
+      case "6XmTjLRFR2w":
+        Script14();
+        break;
+      case "64JjsmsOTid":
+        Script15();
+        break;
+      case "6HD8zeM25CN":
+        Script16();
+        break;
+      case "5VkJO80DsUY":
+        Script17();
+        break;
+      case "5yjG8UzE1Cv":
+        Script18();
+        break;
+      case "6R8jsC6UiEL":
+        Script19();
+        break;
+      case "5kQjxEk2D4W":
+        Script20();
+        break;
+      case "5ZsXFNH109f":
+        Script21();
+        break;
+      case "6q29Ob2pFKt":
+        Script22();
+        break;
+      case "6VadyzUerzb":
+        Script23();
+        break;
+      case "6UhtEwmf1T8":
+        Script24();
+        break;
+      case "6dc53mwOzZK":
+        Script25();
+        break;
+      case "6RqOSGuGe46":
+        Script26();
+        break;
+      case "5pFpNx1vvtY":
+        Script27();
+        break;
+      case "6dpz03xIM8O":
+        Script28();
+        break;
+      case "5aXxilkFMUi":
+        Script29();
+        break;
+      case "5dsRHI4XrxG":
+        Script30();
+        break;
+      case "64Ftlp7cTj7":
+        Script31();
+        break;
+      case "6BNQ01pQQQi":
+        Script32();
+        break;
+  }
+}
+
+window.InitExecuteScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var once = player.once;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+var update = player.update;
+var pointerX = player.pointerX;
+var pointerY = player.pointerY;
+var showPointer = player.showPointer;
+var hidePointer = player.hidePointer;
+var slideWidth = player.slideWidth;
+var slideHeight = player.slideHeight;
+};
---
+++ story_content/user.js
@@ -0,0 +1,368 @@
+window.InitUserScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var once = player.once;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+var update = player.update;
+var pointerX = player.pointerX;
+var pointerY = player.pointerY;
+var showPointer = player.showPointer;
+var hidePointer = player.hidePointer;
+var slideWidth = player.slideWidth;
+var slideHeight = player.slideHeight;
+window.Script1 = function()
+{
+  var player = GetPlayer();
+var audioElements = document.querySelectorAll('audio');
+var isMuted = player.GetVar("isMuted"); // Fetch the variable value
+
+audioElements.forEach(function(audio) {
+    audio.muted = isMuted; // Mute or unmute based on variable
+});
+}
+
+window.Script2 = function()
+{
+  var player = GetPlayer();
+var audioElements = document.querySelectorAll('audio');
+var isMuted = player.GetVar("isMuted"); // Fetch the variable value
+
+audioElements.forEach(function(audio) {
+    audio.muted = isMuted; // Mute or unmute based on variable
+});
+}
+
+window.Script3 = function()
+{
+  var player = GetPlayer();
+var audioElements = document.querySelectorAll('audio');
+var isMuted = player.GetVar("isMuted"); // Fetch the variable value
+
+audioElements.forEach(function(audio) {
+    audio.muted = isMuted; // Mute or unmute based on variable
+});
+}
+
+window.Script4 = function()
+{
+  var player = GetPlayer();
+var audioElements = document.querySelectorAll('audio');
+var isMuted = player.GetVar("isMuted"); // Fetch the variable value
+
+audioElements.forEach(function(audio) {
+    audio.muted = isMuted; // Mute or unmute based on variable
+});
+}
+
+window.Script5 = function()
+{
+  var player = GetPlayer();
+var audioElements = document.querySelectorAll('audio');
+var isMuted = player.GetVar("isMuted"); // Fetch the variable value
+
+audioElements.forEach(function(audio) {
+    audio.muted = isMuted; // Mute or unmute based on variable
+});
+}
+
+window.Script6 = function()
+{
+  var player = GetPlayer();
+var audioElements = document.querySelectorAll('audio');
+var isMuted = player.GetVar("isMuted"); // Fetch the variable value
+
+audioElements.forEach(function(audio) {
+    audio.muted = isMuted; // Mute or unmute based on variable
+});
+}
+
+window.Script7 = function()
+{
+  var player = GetPlayer();
+var audioElements = document.querySelectorAll('audio');
+var isMuted = player.GetVar("isMuted"); // Fetch the variable value
+
+audioElements.forEach(function(audio) {
+    audio.muted = isMuted; // Mute or unmute based on variable
+});
+}
+
+window.Script8 = function()
+{
+  var player = GetPlayer();
+var audioElements = document.querySelectorAll('audio');
+var isMuted = player.GetVar("isMuted"); // Fetch the variable value
+
+audioElements.forEach(function(audio) {
+    audio.muted = isMuted; // Mute or unmute based on variable
+});
+}
+
+window.Script9 = function()
+{
+  var player = GetPlayer();
+var audioElements = document.querySelectorAll('audio');
+var isMuted = player.GetVar("isMuted"); // Fetch the variable value
+
+audioElements.forEach(function(audio) {
+    audio.muted = isMuted; // Mute or unmute based on variable
+});
+}
+
+window.Script10 = function()
+{
+  var player = GetPlayer();
+var audioElements = document.querySelectorAll('audio');
+var isMuted = player.GetVar("isMuted"); // Fetch the variable value
+
+audioElements.forEach(function(audio) {
+    audio.muted = isMuted; // Mute or unmute based on variable
+});
+}
+
+window.Script11 = function()
+{
+  var player = GetPlayer();
+var audioElements = document.querySelectorAll('audio');
+var isMuted = player.GetVar("isMuted"); // Fetch the variable value
+
+audioElements.forEach(function(audio) {
+    audio.muted = isMuted; // Mute or unmute based on variable
+});
+}
+
+window.Script12 = function()
+{
+  var player = GetPlayer();
+var audioElements = document.querySelectorAll('audio');
+var isMuted = player.GetVar("isMuted"); // Fetch the variable value
+
+audioElements.forEach(function(audio) {
+    audio.muted = isMuted; // Mute or unmute based on variable
+});
+}
+
+window.Script13 = function()
+{
+  var player = GetPlayer();
+var audioElements = document.querySelectorAll('audio');
+var isMuted = player.GetVar("isMuted"); // Fetch the variable value
+
+audioElements.forEach(function(audio) {
+    audio.muted = isMuted; // Mute or unmute based on variable
+});
+}
+
+window.Script14 = function()
+{
+  var player = GetPlayer();
+var audioElements = document.querySelectorAll('audio');
+var isMuted = player.GetVar("isMuted"); // Fetch the variable value
+
+audioElements.forEach(function(audio) {
+    audio.muted = isMuted; // Mute or unmute based on variable
+});
+}
+
+window.Script15 = function()
+{
+  var player = GetPlayer();
+var audioElements = document.querySelectorAll('audio');
+var isMuted = player.GetVar("isMuted"); // Fetch the variable value
+
+audioElements.forEach(function(audio) {
+    audio.muted = isMuted; // Mute or unmute based on variable
+});
+}
+
+window.Script16 = function()
+{
+  var player = GetPlayer();
+var audioElements = document.querySelectorAll('audio');
+var isMuted = player.GetVar("isMuted"); // Fetch the variable value
+
+audioElements.forEach(function(audio) {
+    audio.muted = isMuted; // Mute or unmute based on variable
+});
+}
+
+window.Script17 = function()
+{
+  var player = GetPlayer();
+var audioElements = document.querySelectorAll('audio');
+var isMuted = player.GetVar("isMuted"); // Fetch the variable value
+
+audioElements.forEach(function(audio) {
+    audio.muted = isMuted; // Mute or unmute based on variable
+});
+}
+
+window.Script18 = function()
+{
+  var player = GetPlayer();
+var audioElements = document.querySelectorAll('audio');
+var isMuted = player.GetVar("isMuted"); // Fetch the variable value
+
+audioElements.forEach(function(audio) {
+    audio.muted = isMuted; // Mute or unmute based on variable
+});
+}
+
+window.Script19 = function()
+{
+  var player = GetPlayer();
+var audioElements = document.querySelectorAll('audio');
+var isMuted = player.GetVar("isMuted"); // Fetch the variable value
+
+audioElements.forEach(function(audio) {
+    audio.muted = isMuted; // Mute or unmute based on variable
+});
+}
+
+window.Script20 = function()
+{
+  var player = GetPlayer();
+var audioElements = document.querySelectorAll('audio');
+var isMuted = player.GetVar("isMuted"); // Fetch the variable value
+
+audioElements.forEach(function(audio) {
+    audio.muted = isMuted; // Mute or unmute based on variable
+});
+}
+
+window.Script21 = function()
+{
+  var player = GetPlayer();
+var audioElements = document.querySelectorAll('audio');
+var isMuted = player.GetVar("isMuted"); // Fetch the variable value
+
+audioElements.forEach(function(audio) {
+    audio.muted = isMuted; // Mute or unmute based on variable
+});
+}
+
+window.Script22 = function()
+{
+  var player = GetPlayer();
+var audioElements = document.querySelectorAll('audio');
+var isMuted = player.GetVar("isMuted"); // Fetch the variable value
+
+audioElements.forEach(function(audio) {
+    audio.muted = isMuted; // Mute or unmute based on variable
+});
+}
+
+window.Script23 = function()
+{
+  var player = GetPlayer();
+var audioElements = document.querySelectorAll('audio');
+var isMuted = player.GetVar("isMuted"); // Fetch the variable value
+
+audioElements.forEach(function(audio) {
+    audio.muted = isMuted; // Mute or unmute based on variable
+});
+}
+
+window.Script24 = function()
+{
+  var player = GetPlayer();
+var audioElements = document.querySelectorAll('audio');
+var isMuted = player.GetVar("isMuted"); // Fetch the variable value
+
+audioElements.forEach(function(audio) {
+    audio.muted = isMuted; // Mute or unmute based on variable
+});
+}
+
+window.Script25 = function()
+{
+  var player = GetPlayer();
+var audioElements = document.querySelectorAll('audio');
+var isMuted = player.GetVar("isMuted"); // Fetch the variable value
+
+audioElements.forEach(function(audio) {
+    audio.muted = isMuted; // Mute or unmute based on variable
+});
+}
+
+window.Script26 = function()
+{
+  var player = GetPlayer();
+var audioElements = document.querySelectorAll('audio');
+var isMuted = player.GetVar("isMuted"); // Fetch the variable value
+
+audioElements.forEach(function(audio) {
+    audio.muted = isMuted; // Mute or unmute based on variable
+});
+}
+
+window.Script27 = function()
+{
+  var player = GetPlayer();
+var audioElements = document.querySelectorAll('audio');
+var isMuted = player.GetVar("isMuted"); // Fetch the variable value
+
+audioElements.forEach(function(audio) {
+    audio.muted = isMuted; // Mute or unmute based on variable
+});
+}
+
+window.Script28 = function()
+{
+  var player = GetPlayer();
+var audioElements = document.querySelectorAll('audio');
+var isMuted = player.GetVar("isMuted"); // Fetch the variable value
+
+audioElements.forEach(function(audio) {
+    audio.muted = isMuted; // Mute or unmute based on variable
+});
+}
+
+window.Script29 = function()
+{
+  var player = GetPlayer();
+var audioElements = document.querySelectorAll('audio');
+var isMuted = player.GetVar("isMuted"); // Fetch the variable value
+
+audioElements.forEach(function(audio) {
+    audio.muted = isMuted; // Mute or unmute based on variable
+});
+}
+
+window.Script30 = function()
+{
+  var player = GetPlayer();
+var audioElements = document.querySelectorAll('audio');
+var isMuted = player.GetVar("isMuted"); // Fetch the variable value
+
+audioElements.forEach(function(audio) {
+    audio.muted = isMuted; // Mute or unmute based on variable
+});
+}
+
+window.Script31 = function()
+{
+  var player = GetPlayer();
+var audioElements = document.querySelectorAll('audio');
+var isMuted = player.GetVar("isMuted"); // Fetch the variable value
+
+audioElements.forEach(function(audio) {
+    audio.muted = isMuted; // Mute or unmute based on variable
+});
+}
+
+window.Script32 = function()
+{
+  var player = GetPlayer();
+var audioElements = document.querySelectorAll('audio');
+var isMuted = player.GetVar("isMuted"); // Fetch the variable value
+
+audioElements.forEach(function(audio) {
+    audio.muted = isMuted; // Mute or unmute based on variable
+});
+}
+
+};
